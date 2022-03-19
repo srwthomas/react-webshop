ee191ac7fcae54a2824268cc2c16a451ca14882d
Hjalmar-K-sson <45142969+Hjalmar-K-sson@users.noreply.github.com>
2022-03-19 13:38:10 +0100

    created directory page with directory & category item components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import Directory from "./components/Directory/Directory.component.jsx";
+import "./App.sass";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const categories = [
+    {
+      title: "hats",
+      imageUrl:
+        "https://images.unsplash.com/photo-1602337871614-a2bf032f4701?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+      id: 1,
+    },
+    {
+      title: "jackets",
+      imageUrl:
+        "https://images.unsplash.com/photo-1591047139829-d91aecb6caea?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=436&q=80",
+      id: 2,
+    },
+    {
+      title: "sneakers",
+      imageUrl:
+        "https://images.unsplash.com/photo-1588273565869-a06552c35ef5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
+      id: 3,
+    },
+    {
+      title: "womens",
+      imageUrl:
+        "https://images.unsplash.com/photo-1588117260148-b47818741c74?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+      id: 4,
+    },
+    {
+      title: "mens",
+      imageUrl:
+        "https://images.unsplash.com/photo-1516826957135-700dedea698c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+      id: 5,
+    },
+  ];
+  return <Directory categories={categories} />;
+};
 
 export default App;
---
+++ src/components/Directory/Directory.component.jsx
@@ -0,0 +1,14 @@
+import CategoryItem from "../CategoryItem/CategoryItem.component";
+import "./Directory.style.sass";
+
+const Directory = ({ categories }) => {
+  return (
+    <div className="directory-container">
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
+    </div>
+  );
+};
+
+export default Directory;
