bf38f715634c0dc9a336a685f126810630049955
Hjalmar-K-sson <45142969+Hjalmar-K-sson@users.noreply.github.com>
2022-03-23 22:12:59 +0100

    incorporated signUpForm component into SignIn route

--- src/routes/SignIn/SignIn.component.jsx
+++ src/routes/SignIn/SignIn.component.jsx
@@ -1,9 +1,22 @@
+// import { useEffect } from "react";
+// import { getRedirectResult } from "firebase/auth";
 import {
+  // auth,
+  // signInWithGoogleRedirect,
   signInWithGooglePopup,
   createUserDoc,
 } from "../../utils/firebase/firebase.utils";
 
+import SignUpForm from "../../components/SignUpForm/SignUpForm.component";
+
 const SignIn = () => {
+  // useEffect(async () => {
+  //   const response = await getRedirectResult(auth);
+  //   if (response) {
+  //     const userDocRef = await createUserDoc(response.user);
+  //   }
+  // }, []);
+
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDoc(user);
@@ -11,7 +24,11 @@ const SignIn = () => {
   return (
     <div>
       <h1>Sign in page</h1>
-      <button onClick={logGoogleUser}>Sign in with Google</button>
+      <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+      <SignUpForm />
+      {/* <button onClick={signInWithGoogleRedirect}>
+        Sign in with Google Redirect
+      </button> */}
     </div>
   );
 };
