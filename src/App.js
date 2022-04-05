20581863e2b49f288c0e598bd1e2e0953c972189
Hjalmar-K-sson <45142969+Hjalmar-K-sson@users.noreply.github.com>
2022-04-05 17:47:00 +0200

    updated Auth component import + updated auth route path

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./routes/Home/Home.component.jsx";
 import Navigation from "./routes/Navigation/Navigation.component.jsx";
-import SignIn from "./routes/SignIn/SignIn.component.jsx";
+import Auth from "./routes/Auth/Auth.component.jsx";
 
 const Shop = () => {
   return <h1>Shop link</h1>;
@@ -13,7 +13,7 @@ const App = () => {
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
-        <Route path="sign-in" element={<SignIn />} />
+        <Route path="auth" element={<Auth />} />
       </Route>
     </Routes>
   );
