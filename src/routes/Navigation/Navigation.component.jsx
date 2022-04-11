07008507030842a2d10c8688692147e6a7b487d8
Hjalmar-K-sson <45142969+Hjalmar-K-sson@users.noreply.github.com>
2022-04-11 20:27:03 +0200

    removed unnecessary user setter function, removed signOutHandler function leaving only necessary signOutUser helper function

--- src/routes/Navigation/Navigation.component.jsx
+++ src/routes/Navigation/Navigation.component.jsx
@@ -9,12 +9,8 @@ import { UserContext } from "../../contexts/User.context";
 import "./Navigation.style.sass";
 
 const Navigation = () => {
-  const { currentUser, setCurrentUser } = useContext(UserContext);
-  console.log(currentUser);
-  const signOutHandler = async () => {
-    await signOutUser();
-    setCurrentUser(null);
-  };
+  const { currentUser } = useContext(UserContext);
+
   return (
     <Fragment>
       <div className="navigation">
@@ -26,7 +22,7 @@ const Navigation = () => {
             SHOP
           </Link>
           {currentUser ? (
-            <span className="nav-link" onClick={signOutHandler}>
+            <span className="nav-link" onClick={signOutUser}>
               SIGN OUT
             </span>
           ) : (
