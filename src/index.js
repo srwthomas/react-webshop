db540bf6fbfab43a783721c45bca26b7eee63938
Hjalmar-K-sson <45142969+Hjalmar-K-sson@users.noreply.github.com>
2022-04-12 20:50:27 +0200

    imported Products & Cart context providers, wrapped App in ProductsProvider & CartProvider

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 
 import { UserProvider } from "./contexts/User.context";
+import { ProductsProvider } from "./contexts/Products.context";
+import { CartProvider } from "./contexts/Cart.context";
 import App from "./App";
 
 import "./index.sass";
@@ -11,7 +13,11 @@ render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <App />
+        <ProductsProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </ProductsProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>,
