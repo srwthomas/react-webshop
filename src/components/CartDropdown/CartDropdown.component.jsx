4515a12d8c59d304d385cb82197ea83b56c03de2
Hjalmar-K-sson <45142969+Hjalmar-K-sson@users.noreply.github.com>
2022-05-19 16:56:01 +0200

    added cart context to keep track of cart state

--- src/components/CartDropdown/CartDropdown.component.jsx
+++ src/components/CartDropdown/CartDropdown.component.jsx
@@ -1,12 +1,21 @@
+import { useContext } from "react";
+
+import { CartContext } from "../../contexts/Cart.context";
+
 import { Button } from "../Button/Button.component";
 import CartItem from "../CartItem/CartItem.component";
 
 import "./CartDropdown.style.sass";
 
 const CartDropdown = () => {
+  const { cartItems } = useContext(CartContext);
   return (
     <div className="cart-dropdown-container">
-      <div className="cart-items">{}</div>
+      <div className="cart-items">
+        {cartItems.map((cartItem) => (
+          <CartItem key={cartItem.id} cartItem={cartItem} />
+        ))}
+      </div>
       <Button>GO TO CHECKOUT</Button>
     </div>
   );
