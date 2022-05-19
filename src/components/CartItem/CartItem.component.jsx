b8ea41fe3037e5987cc1add0ff28ee76df36b5b1
Hjalmar-K-sson <45142969+Hjalmar-K-sson@users.noreply.github.com>
2022-05-19 17:24:43 +0200

    displayed product img, added cart-item-container class

--- src/components/CartItem/CartItem.component.jsx
+++ src/components/CartItem/CartItem.component.jsx
@@ -1,9 +1,10 @@
 import "./CartItem.style.sass";
 
 const CartItem = ({ cartItem }) => {
-  const { name, quantity } = cartItem;
+  const { name, quantity, imageUrl } = cartItem;
   return (
-    <div>
+    <div className="cart-item-container">
+      <img src={imageUrl} alt={name}></img>
       <h2>{name}</h2>
       <span>{quantity}</span>
     </div>
