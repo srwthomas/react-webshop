908fa5f3f09c3dc284a16dcf9cf6ef89bcc7c37b
Hjalmar-K-sson <45142969+Hjalmar-K-sson@users.noreply.github.com>
2022-05-21 12:47:41 +0200

    updated layout, added price info display

--- src/components/CartItem/CartItem.component.jsx
+++ src/components/CartItem/CartItem.component.jsx
@@ -1,12 +1,16 @@
 import "./CartItem.style.sass";
 
 const CartItem = ({ cartItem }) => {
-  const { name, quantity, imageUrl } = cartItem;
+  const { name, quantity, price, imageUrl } = cartItem;
   return (
     <div className="cart-item-container">
       <img src={imageUrl} alt={name}></img>
-      <h2>{name}</h2>
-      <span>{quantity}</span>
+      <div className="item-details">
+        <span className="name">{name}</span>
+        <span className="price">
+          {quantity} x ${price}
+        </span>
+      </div>
     </div>
   );
 };
