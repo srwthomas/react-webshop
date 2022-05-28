cedecbf724f035a8f6a2d9d4bf1de8628dafec9e
Hjalmar-K-sson <45142969+Hjalmar-K-sson@users.noreply.github.com>
2022-05-28 14:24:00 +0200

    changed ProductsContext into CategoriesContext, adjusted to facilitate a more complex set of mock up data containing multiple product categories

---
+++ src/contexts/Categories.context.jsx
@@ -0,0 +1,36 @@
+import { createContext, useState, useEffect } from "react";
+
+//BELOW WE HAVE IMPORTS REGARDING CHANGES TO THE PRODUCTS DB
+// import { addCollectionAndDocuments } from "../utils/firebase/firebase.utils";
+// import SHOP_DATA from "../shop-data";
+
+import { getCategoriesAndDocuments } from "../utils/firebase/firebase.utils";
+
+export const CategoriesContext = createContext({
+  categoriesMap: {},
+});
+
+export const CategoriesProvider = ({ children }) => {
+  const [categoriesMap, setCategoriesMap] = useState({});
+
+  //ONLY USE WHEN YOU WANTO TO MAKE CHANGES TO THE PRODUCTS DB!!!
+  // useEffect(() => {
+  //   addCollectionAndDocuments("categories", SHOP_DATA);
+  // }, []);
+
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const categoryMap = await getCategoriesAndDocuments();
+      console.log(categoryMap);
+      setCategoriesMap(categoryMap);
+    };
+    getCategoriesMap();
+  }, []);
+
+  const value = { categoriesMap };
+  return (
+    <CategoriesContext.Provider value={value}>
+      {children}
+    </CategoriesContext.Provider>
+  );
+};
