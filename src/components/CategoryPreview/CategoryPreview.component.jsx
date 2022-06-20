8abc37eadde8750a44104514a6fafc5b3473fa97
Hjalmar-K-sson <45142969+Hjalmar-K-sson@users.noreply.github.com>
2022-06-20 18:20:00 +0200

    created shop page category preview component

---
+++ src/components/CategoryPreview/CategoryPreview.component.jsx
@@ -0,0 +1,22 @@
+import "./CategoryPreview.style.sass";
+
+import ProductCard from "../ProductCard/ProductCard.component";
+
+const CategoryPreview = ({ title, products }) => {
+  return (
+    <div className="category-preview-container">
+      <h2>
+        <span className="title">{title.toUpperCase()}</span>
+      </h2>
+      <div className="preview">
+        {products
+          .filter((_, index) => index < 4)
+          .map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default CategoryPreview;
