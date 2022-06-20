c4ae3416bab8ce0261a22dcd427aee785a569c60
Hjalmar-K-sson <45142969+Hjalmar-K-sson@users.noreply.github.com>
2022-06-20 20:01:04 +0200

    updated import & rendered component naming after DirectoryItem rename

--- src/components/Directory/Directory.component.jsx
+++ src/components/Directory/Directory.component.jsx
@@ -1,11 +1,11 @@
-import CategoryItem from "../CategoryItem/CategoryItem.component";
+import DirectoryItem from "../DirectoryItem/DirectoryItem.component";
 import "./Directory.style.sass";
 
 const Directory = ({ categories }) => {
   return (
     <div className="directory-container">
       {categories.map((category) => (
-        <CategoryItem key={category.id} category={category} />
+        <DirectoryItem key={category.id} category={category} />
       ))}
     </div>
   );
