980d5c17e190b4490698d4f067f7631d44249e1d
Hjalmar-K-sson <45142969+Hjalmar-K-sson@users.noreply.github.com>
2022-06-26 13:31:18 +0200

    removed UserProvider, added redux Provider

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from "react";
 import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 
-import { UserProvider } from "./contexts/User.context";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
+
 import { CategoriesProvider } from "./contexts/Categories.context";
 import { CartProvider } from "./contexts/Cart.context";
 import App from "./App";
@@ -11,15 +13,15 @@ import "./index.sass";
 
 render(
   <React.StrictMode>
-    <BrowserRouter>
-      <UserProvider>
+    <Provider store={store}>
+      <BrowserRouter>
         <CategoriesProvider>
           <CartProvider>
             <App />
           </CartProvider>
         </CategoriesProvider>
-      </UserProvider>
-    </BrowserRouter>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
