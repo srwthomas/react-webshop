ac9127319cdce35a8c23b165cc918a96c6e08dc8
Hjalmar-K-sson <45142969+Hjalmar-K-sson@users.noreply.github.com>
2022-06-29 20:10:54 +0200

    created simplified logger middleware for eventual use

--- src/store/store.js
+++ src/store/store.js
@@ -4,6 +4,20 @@ import logger from "redux-logger";
 
 import { rootReducer } from "./root-reducer";
 
+// Our own logger middleware
+// const loggerMiddleware = (store) => (next) => (action) => {
+//   if (!action.type) {
+//     next(action);
+//   }
+//   console.log("type: ", action.type);
+//   console.log("payload: ", action.payload);
+//   console.log("current tate: ", store.getState());
+
+//   next(action);
+
+//   console.log("next state: ", store.getState());
+// };
+
 // const middleware = [logger];
 // const composedEnhancers = compose(applyMiddleware(...middleware));
 
